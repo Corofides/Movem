71aa4e2b0c16d424db81aaf908bec379a61d30ac
James Lee Lendrem <J.l.lendrem@smoothlines.net>
2023-02-07 22:31:27 +0000

    Fix the issue with the opposite direction. Add comment so this isn't the laziest commit in the universe.

--- app/src/commonMain/kotlin/Commands/MoveCommand.kt
+++ app/src/commonMain/kotlin/Commands/MoveCommand.kt
@@ -12,8 +12,10 @@ class MoveCommand (
     private val actor: Moveable = actor;
     private var direction: Direction = direction;
 
-    //Temping this as otherwise intellij is going to waste an hour indexing the project for some fucking reason.
-    private var oppositeDirection: Direction = Direction.SOUTH; // = (direction + 2) % 4;
+    // Converting the direction to the opposite direction. Since we have 4 values 0, 1, 2, 3 and the
+    // difference between the opposites is 2, we can just add 2 and mod by 4 to get the correct value.
+    // This will allow us to undo our move if we ever need this.
+    private var oppositeDirection: Direction = Direction.values()[(direction.ordinal + 2) % 4];
 
     //private var _previousX: Double = 0.0;
     //private var _previousY: Double = 0.0;
