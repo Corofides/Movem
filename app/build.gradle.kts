7369c63c1432564e235c6342b250717131a089cb
Alex Hedley <alex.hedley@me.com>
2023-06-15 19:09:30 +0100

    Added Icon
    Co-authored-by: Corofides <J.l.lendrem@smoothlines.net>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,6 +10,42 @@ plugins {
 
 korge {
 	id = "com.example.movem"
+    version = "0.0.1"
+//    exeBaseName = "app"
+    name = "Movem"
+    description = "Movem Game"
+//    orientation = Orientation.DEFAULT
+//    copyright = "Copyright (c) 2023 Unknown"
+//
+//    // Configuring the author
+//    authorName = "Corofides"
+//    authorEmail = "unknown@unknown"
+//    authorHref = "https://github.com/Corofides"
+//    author("name", "email", "href")
+//
+//    authorName = "Alex Hedley"
+//    authorEmail = "unknown@unknown"
+//    authorHref = "https://github.com/AlexHedley"
+//    author("name", "email", "href")
+//
+    icon = File(rootDir, "src/commonMain/resources/Icon.png")
+//
+    gameCategory = GameCategory.ARCADE
+//    fullscreen = true
+//    backgroundColor = 0xff000000.toInt()
+//    appleDevelopmentTeamId = java.lang.System.getenv("DEVELOPMENT_TEAM") ?: project.findProperty("appleDevelopmentTeamId")?.toString()
+//    appleOrganizationName = "User Name Name"
+//    entryPoint = "main"
+//    jvmMainClassName = "MainKt"
+//    androidMinSdk = null
+//
+//    //androidAppendBuildGradle("...code...")
+//    config("MYPROP", "MYVALUE")
+//
+//    // Korge Plugins
+//    plugin("com.soywiz:korge-admob:$korgeVersion", mapOf("ADMOB_APP_ID" to ADMOB_APP_ID))
+//    admob(ADMOB_APP_ID) // Shortcut for admob
+
 // To enable all targets at once
 
 	//targetAll()
